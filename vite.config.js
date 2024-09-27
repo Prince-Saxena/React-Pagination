a844fc34ef2b8c466862babe0adaa4ebb2236f34
Prince Saxena <princesaxena202020@gmail.com>
2024-09-27 21:08:02 +0530

    reinit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 // vite.config.js
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	build: {
-		outDir: "build", // Change this from 'dist' to 'build'
-	},
+	plugins: [react()],
+	base: "/React-Pagination/", // Replace <repository-name> with your actual repo name
 });
