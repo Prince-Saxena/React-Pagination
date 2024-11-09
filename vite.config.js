e1e849f1a8241b5685b540742e58f1a1cbf4333e
Prince Saxena <princesaxena202020@gmail.com>
2024-11-09 10:59:53 +0530

    some issue fixes of index html

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	plugins: [react()],
-	base: "/React-Pagination/", // Replace <repository-name> with your actual repo name
+	base: "/React-Pagination/", // set to your repository name
+	plugins: [react()], // Replace <repository-name> with your actual repo name
 });
